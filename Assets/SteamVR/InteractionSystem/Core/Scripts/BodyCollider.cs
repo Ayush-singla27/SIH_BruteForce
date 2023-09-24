7cef4678cef4292880fa65a4175d0c84013b2486
Ayush Singla <ayushsingla2703@gmail.com>
2023-09-24 20:48:19 +0530

    done

---
+++ Assets/SteamVR/InteractionSystem/Core/Scripts/BodyCollider.cs
@@ -0,0 +1,20 @@
+﻿//======= Copyright (c) Valve Corporation, All rights reserved. ===============
+//
+// Purpose: Collider dangling from the player's head
+//
+//=============================================================================
+
+using UnityEngine;
+
+namespace Valve.VR.InteractionSystem
+{
+    public class BodyCollider : MonoBehaviour
+    {
+        [SerializeField] private Transform _head;
+
+        private void FixedUpdate()
+        {
+            transform.localRotation = _head.localRotation;
+        }
+    }
+}
